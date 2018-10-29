947ecb3935d5519b0d0009987c1d24629d0c60d5
lyz0907 <yazhou6543@gmail.com>
2018-10-29 10:28:49 +0800

    读写多行数据

--- python-base/io/file/read_write_lines.py
+++ python-base/io/file/read_write_lines.py
@@ -1,15 +1,24 @@
 
-FILE_PATH = r"D:\work_material\project-document\RTDGW-实时数据网关\生产环境topic提数\jdqDataExtract_2018072412-2018072512.txt"
-
 rows = []
-with open(FILE_PATH, 'r') as stat_file:
-    counter = 0
-    for line in stat_file:
-        rows.append(line)
-        counter += 1
-        if counter == 10:
-            break
 
-DIST_PATH = r"D:\work_material\project-document\RTDGW-实时数据网关\生产环境topic提数\jdqDataExtract_2018072412-2018072512_head10.txt"
-with open(DIST_PATH, 'w') as file:
-    file.writelines(rows)
+
+def read_rows():
+    FILE_PATH = r"src_file.txt"
+    counter = 0
+    with open(FILE_PATH, 'r') as stat_file:
+        for line in stat_file:
+            # rows.append(line)
+            counter += 1
+            if counter == 1 or counter == 1000 or counter == 2000 or counter == 3000 or counter == 4000 or counter == 4924:
+                print(counter, ", ", line)
+    print("counter = ", counter)
+
+
+def write_rows():
+    DIST_PATH = r"dist_file.txt"
+    with open(DIST_PATH, 'w') as file:
+        file.writelines(rows)
+
+
+if __name__ == '__main__':
+    read_rows()
