b81301a6b1dcc28d47d04a990a22a89e47a412eb
liyazhou <yazhou6543@gmail.com>
2020-07-23 20:13:29 +0800

    machine-learning代码整理

--- machine_learning/DataMiningCompetitionFirstPrize-master/data_analysis/user_record.py
+++
@@ -1,7 +0,0 @@
-file_loc = "/Users/mac/Documents/contest/data/original_data/score_train.txt"
-user_id_set=set()
-lines = open(file_loc).readlines()
-for line in lines:
-    user_id_set.add(line.split(",")[0])
-print len(user_id_set)
-
--- machine_learning/tianchiCode-master/passenger_predict/compute_error.py
+++
@@ -1,4 +0,0 @@
-__author__ = 'burness'
-def compute_error(predict_labels, test_labels):
-    error = abs(test_labels-predict_labels)/test_labels
-    return error.mean()
